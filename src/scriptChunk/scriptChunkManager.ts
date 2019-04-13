ddaf762770159d5da5b4d7c31c3c2731113c5aae
negokaz <negokaz@gmail.com>
2019-04-13 22:22:41 +0900

    ✨feat: use  md5 of script-chunk instead of uuidv4 for script-chunk-id

--- src/scriptChunk/scriptChunkManager.ts
+++ src/scriptChunk/scriptChunkManager.ts
@@ -1,6 +1,7 @@
 import * as uuidv4 from 'uuid/v4';
 import { Token } from 'markdown-it';
 import ScriptChunk from './scriptChunk';
+import * as crypto from 'crypto';
 
 export default class ScriptChunkManager {
 
@@ -19,13 +20,26 @@ export default class ScriptChunkManager {
             if (token.type === 'fence') {
                 const chunk = ScriptChunk.parse(token);
                 if (chunk.isRunnable) {
-                    const scriptChunkId = uuidv4();
+                    const scriptChunkId = this.generateScriptChunkId(chunk);
                     token.attrSet(ScriptChunkManager.SCRIPT_CHUNK_ID_ATTR_NAME, scriptChunkId);
                     this.scriptChunks.set(scriptChunkId, chunk);
                 }
             }
             return token;
         });  
+    }
+
+    generateScriptChunkId(scriptChunk: ScriptChunk): string {
+        let scriptChunkId = '';
+        for (let i = 0; scriptChunkId.length === 0 || this.scriptChunks.has(scriptChunkId); i++) {
+            const md5 = crypto.createHash('md5');
+            md5.update(scriptChunk.cmd);
+            md5.update(scriptChunk.args.join(' '));
+            md5.update(scriptChunk.script);
+            md5.update(i.toString());
+            scriptChunkId = md5.digest('hex');
+        }
+        return scriptChunkId;
     }
 
     public getScriptChunk(scriptChunkId: string): ScriptChunk {
