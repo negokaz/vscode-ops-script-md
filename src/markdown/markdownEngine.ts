2018d51054d471175bc5e0582365dca440b13ee8
negokaz <negokaz@gmail.com>
2019-06-02 19:12:26 +0900

    ✨feat: enable some options of markdown-it

--- src/markdown/markdownEngine.ts
+++ src/markdown/markdownEngine.ts
@@ -12,6 +12,9 @@ export default class MarkdownEngine {
 
     constructor() {
         this.md = new MarkdownIt({
+            html:         true,        // Enable HTML tags in source
+            breaks:       true,        // Convert '\n' in paragraphs into <br>
+            linkify:      true,        // Autoconvert URL-like text to links
             highlight: (str, lang) => {
                 if (lang && hljs.getLanguage(lang)) {
                     try {
